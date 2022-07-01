53995e6f9005cc78dd7d9f9991641e76db6f65b6
Francisco-Gorgonho <francisco.gorgonho@gmail.com>
2022-07-01 13:08:40 -0300

    Aplicando o framework do CSS3 Bootstrap e linguagem de programação Javascript

---
+++ UC9SA2_encontroremoto2/js/script.js
@@ -0,0 +1,12 @@
+function cadastroEnviado() {
+    alert("Formulário enviado com sucesso")
+}
+
+function cadastrarNewsletter() {
+
+    let email = document.getElementById("email-newsletter").value;
+
+    alert(email)
+
+    console.log(email)
+}
