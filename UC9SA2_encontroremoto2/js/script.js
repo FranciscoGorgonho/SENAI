e48e838b0eea17e41677ca10387fdc2c7ad29d78
Francisco-Gorgonho <francisco.gorgonho@gmail.com>
2022-07-04 12:28:17 -0300

    Incluindo funções do Javascript Jquery

--- UC9SA2_encontroremoto2/js/script.js
+++ UC9SA2_encontroremoto2/js/script.js
@@ -1,3 +1,33 @@
+$(document).ready(function() {
+
+    let slideAtual = 1;
+    let listaSlides = ["banner-c", "banner-ti", "banner-d"]
+
+    setInterval(mudarSlide, 1000)
+
+    function mudarSlide() {
+        console.log("Slide atual:", slideAtual);
+       
+        // Remove o slide anterior
+        if (slideAtual > 0) {
+            $("#carrosel").removeClass(listaSlides[slideAtual -1])
+        } else if (slideAtual == 0) {
+            $("#carrosel").removeClass(listaSlides[ (listaSlides.lenght) -1])
+        }
+    
+        // Adiciona o slide atual
+        $("#carrosel").addClass(listaSlides[slideAtual]);
+    
+        // Indica o slide atual
+        slideAtual++
+
+        if(slideAtual > (listaSlides.length) -1) {
+            slideAtual = 0;
+        }
+
+}
+})
+
 function cadastroEnviado() {
     alert("Formulário enviado com sucesso")
 }
@@ -6,7 +36,7 @@ function cadastrarNewsletter() {
 
     let email = document.getElementById("email-newsletter").value;
 
-    alert(email)
+    alert("e-mail cadastrado com sucesso")
 
     console.log(email)
 }
