2580b261bc92a80420c068e69e49e26e0c0b6a6e
Francisco-Gorgonho <francisco.gorgonho@gmail.com>
2022-10-22 11:17:18 -0300

    Encontro remoto 04

--- UC12/BE7_FS4_UC9/Classes/PessoaFisica.cs
+++ UC12/BE7_FS4_UC9/Classes/PessoaFisica.cs
@@ -1,44 +1,48 @@
-using BE7_FS4_UC9.Interfaces;
+ using BE7_FS4_UC9.Interfaces;
 
 namespace BE7_FS4_UC9.Classes
 {
     public class PessoaFisica : Pessoa, IPessoaFisica
     {
         public string ?cpf { get; set; }
-        public string ?dataNascimento { get; set; }    
-        
+        public string ?dataNascimento { get; set; }
         
         public override float PagarImposto(float rendimento)
         {
             throw new NotImplementedException();
         }
-
         
-        public bool ValidarDataNascimento(DateTime dataNasc)
+         /*
+        public bool ValidarDataNascimento(DateTime dataNasc) //check > or < 18 years old
         {
             DateTime dataAtual = DateTime.Today;
             double anos = (dataAtual - dataNasc).TotalDays /365;
+            Console.WriteLine($"Data atual: {dataAtual} \nAnos: {anos}");
+            
             if(anos >= 18){
                 return true;
-            }
-            return false;
-        }
-        
-
-        public bool ValidarDataNascimento(string dataNasc)
-        {
-            DateTime dataConvertida;
-            //verificar se a string esta em um formado valido
-            if(DateTime.TryParse(dataNasc, out dataConvertida)){//tryParse tenta converter e coloca na saida out 
-                //Console.WriteLine($"{dataConvertida}");
-                DateTime dataAtual = DateTime.Today;
-                double anos = (dataAtual - dataConvertida).TotalDays /365;
-                if(anos >= 18){
-                    return true;
-                }
+            }else{
                 return false;
             }
+            
+        }
+        */
+        public bool ValidarDataNascimento(string dataNasc) //check > or < 18 years old
+        {
+            DateTime dataConvertida;
+            if(DateTime.TryParse(dataNasc, out dataConvertida)){
+                //Console.WriteLine($"{dataConvertida}");
+                
+                DateTime dataAtual = DateTime.Today;
+                double anos = (dataAtual - dataConvertida).TotalDays /365;
+                Console.WriteLine($"Data atual: {dataAtual} \nAnos: {anos}");
+                if(anos >= 18){
+                    return true;
+                }else{
+                    return false;
+                }  
+            }
             return false;            
-        } 
+        }
     }
 }
--- UC12/BE7_FS4_UC9/Program.cs
+++ UC12/BE7_FS4_UC9/Program.cs
@@ -2,6 +2,7 @@
 
 using BE7_FS4_UC9.Classes;
 
+/*
 PessoaFisica novaPf = new PessoaFisica();
 Endereco novoEnd = new Endereco();
 
@@ -24,9 +25,7 @@ Nome: {novaPf.nome}
 Endereco: {novaPf.endereco.logradouro}, {novaPf.endereco.numero}
 Maior de idade: {metodoPf.ValidarDataNascimento(novaPf.dataNascimento)}
 ");
-
-
-
+*/
 /*
 novaPf.nome = "Lozano";
 Console.WriteLine(novaPf.nome);
@@ -35,4 +34,27 @@ Console.WriteLine($"Nome: {novaPf.nome}");
 */
 
 //Console.WriteLine(novaPf.ValidarDataNascimento(new DateTime(2000,01,01)));
-//Console.WriteLine(novaPf.ValidarDataNascimento("01-01-2000"));
+
+//*Console.WriteLine(novaPf.ValidarDataNascimento(new DateTime(2000,01,01)));
+
+PessoaJuridica metodoPj = new PessoaJuridica();
+PessoaJuridica novaPj = new PessoaJuridica();
+Endereco novoEndPj = new Endereco();
+
+novaPj.nome ="NomePj";
+novaPj.cnpj ="00000000000100";
+novaPj.razaoSocial ="Razao Social Pj";
+novaPj.rendimento = 8000.5f;
+
+novoEndPj.logradouro = "Alameda Barao de Limeira";
+novoEndPj.numero = 539;
+novoEndPj.complemento = "Senai Informática";
+novoEndPj.endComercial = true;
+novaPj.endereco = novoEndPj;
+
+Console.WriteLine(@$"
+Nome: {novaPj.nome}
+Razao Social: {novaPj.razaoSocial}
+CNPJ: {novaPj.cnpj}
+CNPJ é válido: {metodoPj.ValidarCnpj(novaPj.cnpj)}");
+
