73d56a1e96c6c6f04593ad18d97364f5e6a9ea5b
Francisco-Gorgonho <francisco.gorgonho@gmail.com>
2022-11-02 16:46:04 -0300

    Atividade onlinee 2, parte 1 de 4

---
+++ UC14/Atividade_Online_2/ExoApi/ExoApi/ExoApi/Contexts/dbExoAPIContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ExoApi.Models;
+
+namespace ExoApi.Contexts
+{
+    public class dbExoAPIContext: DbContext
+    {
+        public dbExoAPIContext()
+        {
+        }
+
+        public dbExoAPIContext(DbContextOptions<dbExoAPIContext> options) : base(options)
+        {
+        }
+
+             protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = DESKTOP-R4IJFCJ\\SQLEXPRESS; initial catalog = dbExoAPI; User Id = sa; Password = Admin123 ");
+            }
+        }
+            public DbSet<Projeto> Projetos { get; set; }
+    }
+}
